62042b14fc09278e8259eb5d9500b118617dca18
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-13 11:31:55 +0200

    EXO 1 mutualisation du code

--- td1.php
+++ td1.php
@@ -4,20 +4,21 @@
 <header></header>
 <body>
 <?php
-function start_page($title)
-{
-    echo ' <!DOCTYPE html> <html
-lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
-    ;
-};
+echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
+echo 'C\'est pas mal non ? <br/>';
 ?>
 <?php
-function start_page($title)
-{
-    echo ' <!DOCTYPE html> <html
-lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
-    ;
-};
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
+?>
+<?php echo "$var1 + $var2"; ?>
+<br/>
+<?php echo $var1 + $var2; ?>
+<br/>
+<br/>
+<?php
+start_page('titre');
 ?>
 </body>
 
---
+++ test.php
@@ -0,0 +1,27 @@
+<?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
+?>
+<?php
+function end_page
+{
+    echo'<body></body><html/>'
+}
+?>
+
+<!DOCTYPE>
+<HTML>
+
+<HEADER>
+<title>Titre</title>
+</HEADER>
+<body>
+<hr/><br/><strong>Test</strong><br/><hr/>
+</body>
+
+</HTML>
+
