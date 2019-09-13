51225b4a26a52c17e36a19c60f12feb4afeb667d
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-13 11:39:47 +0200

    EXO 1 mutualisation du code

--- test.php
+++ test.php
@@ -9,23 +9,16 @@ lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
 <?php
 start_page('titre');
 ?>
-<!DOCTYPE>
-<HTML>
 
-<HEADER>
-    <title>Titre</title>
-</HEADER>
-<body>
 <hr/><br/><strong>Test</strong><br/><hr/>
-</body>
-
-</HTML>
 
 <?php
-function end_page
+function end_page()
 {
-    echo'<body></body><html/>';
+    echo'</body><html/>';
 };
 ?>
 
-
+<?php
+end_page();
+?>
