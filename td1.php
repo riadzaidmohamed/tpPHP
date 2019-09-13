2f7a3d2334d4ae257d86025b4e0ffea990d60ed9
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-13 10:53:40 +0200

    EXO 1

--- td1.php
+++ td1.php
@@ -1,7 +1,19 @@
 <!DOCTYPE HTML>
 <HTML>
+
+<header></header>
+<body>
 <?php
 echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
 echo 'C\'est pas mal non ? <br/>';
 ?>
+<?php
+$var1 = 6;
+$var2 = 1.3;
+$var3 = 'Variable 3';
+?>
+<?php echo "$var1 + $var2"; ?>
+<?php echo $var1 + $var2; ?>
+</body>
+
 </HTML>
