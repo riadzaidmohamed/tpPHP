b39989c8b13cf4708484ac8cc76e9265f027b087
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-13 11:08:25 +0200

    EXO 1 mutualisation du code

--- td1.php
+++ td1.php
@@ -4,19 +4,13 @@
 <header></header>
 <body>
 <?php
-echo '<strong> Voici mon premier programme PHP </strong><br/>' . "\n";
-echo 'C\'est pas mal non ? <br/>';
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
 ?>
-<?php
-$var1 = 6;
-$var2 = 1.3;
-$var3 = 'Variable 3';
-?>
-<?php echo "$var1 + $var2"; ?>
-<br/>
-<?php echo $var1 + $var2; ?>
-<br/>
-<br/>
 <?php
 function start_page($title)
 {
