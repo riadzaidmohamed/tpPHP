d63a3e33beacd8f6e8c72635b2a84892993908c9
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-25 17:01:53 +0200

    exo 1-6 Factorisation du code

--- calculatrice.php
+++ calculatrice.php
@@ -21,17 +21,22 @@ $operateurs = '*+-/';
 <?php
 start_page('calculatrice');
 ?>
-<?php
-for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
-{
-    echo '<input ';
-    if($cpt == 0)
+<form action="calcul.php" method="post">
+    <input type="text" name="op1"><br>
+    <input type="text" name="op2" ><br>
+    <?php
+    for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
     {
-        echo 'checked="checked" ';
-    }
-    echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
+        echo '<input ';
+        if($cpt == 0)
+        {
+            echo 'checked="checked" ';
+        }
+        echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
 }
-?>
+    ?>
+        <input type="submit" value="Submit">
+</form>
 
 <?php
 end_page();
