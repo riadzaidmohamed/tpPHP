4c2ac8d32538e76563a8043d138f8a658fc0a5f2
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-25 16:38:08 +0200

    exo 1 calcul

--- calcul.php
+++ calcul.php
@@ -21,10 +21,31 @@ $op2 = $_POST['op2'];
 $op = $_POST['op'];
 ?>
 
-<?php
-echo "op1 * op2 ="
-?>
 
+<?php
+if('*' == $op)
+{
+    echo $op1*$op2;
+}
+elseif('+' == $op)
+{
+    echo $op1+$op2;
+}
+elseif('-' == $op)
+{
+    echo $op1-$op2;
+}
+elseif ('/'== $op)
+{
+    echo $op1/$op2;
+}
+
+else
+{
+    echo '<br/><strong>opérateur ' . $op . ' non géré </strong>';
+}
+?>
+<br>
 <?php
 end_page();
 ?>
