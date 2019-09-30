8f94e10c9852f0b6b1c64d9417d789bdf6130f41
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-30 09:33:32 +0200

    td2

---
+++ test.inc.php
@@ -0,0 +1,3 @@
+<?php
+include 'utils.inc.php';
+?>
---
+++ utils.inc.php
@@ -0,0 +1,28 @@
+<?php
+function start_page($title)
+{
+    echo ' <!DOCTYPE html> <html
+lang="fr"><head><title>' . PHP_EOL . $title . '</title></head><body>' . PHP_EOL
+    ;
+};
+?>
+
+<?php
+function end_page()
+{
+    echo'</body></html>';
+};
+?>
+
+<?php
+start_page('calcul');
+?>
+
+
+
+
+
+
+<?php
+end_page();
+?>
