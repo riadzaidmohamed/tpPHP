15ad7f932b2a2c29910f7db056be1b6c2b6f48cd
z18010849 <riad.zaid-mohamed@etu.univ-amu.fr>
2019-09-30 08:42:44 +0200

    exo 1-6 Factorisation du code

--- calculatrice.php
+++ calculatrice.php
@@ -25,15 +25,10 @@ start_page('calculatrice');
     <input type="text" name="op1"><br>
     <input type="text" name="op2" ><br>
     <?php
-    for($cpt = 0 ; $cpt <= 3 ; ++$cpt)
-    {
-        echo '<input ';
-        if($cpt == 0)
-        {
-            echo 'checked="checked" ';
-        }
-        echo 'type="radio" name="op" value="' . $operateurs[$cpt] . '"/>' . $operateurs[$cpt] . ' <br/>' . "\n";
-}
+        <input type="submit" value="*" name="Action">
+        <input type="submit" value="+" name="Action">
+        <input type="submit" value="/" name="Action">
+        <input type="submit" value="-" name="Action">
     ?>
         <input type="submit" value="Submit" name="Action">
 </form>
